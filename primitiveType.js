a6a478032c8ba748d7f8b9e578d70c587c6a128c
charan04k <charankarthick04@gmail.com>
2023-04-26 09:58:56 +0530

    first commit

---
+++ primitiveType.js
@@ -0,0 +1,8 @@
+var x = 123245;
+var y = "charan";
+var z = true;
+console.log(x, y, z);
+var a;
+a = 1;
+a = "charan";
+console.log(typeof (a));
---
+++ primitiveType.ts
@@ -0,0 +1,8 @@
+var x:number=12_32_45
+var y:string="charan"
+var z:boolean=true
+console.log(x,y,z);
+var a ;
+a=1
+a="charan"
+console.log(typeof(a));
