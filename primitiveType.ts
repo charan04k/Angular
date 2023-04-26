4b0193142381b89d6b5fff7ebcb16c8ab210f718
charan04k <charankarthick04@gmail.com>
2023-04-26 10:26:06 +0530

    arrays commit 1

--- primitiveType.ts
+++ primitiveType.ts
@@ -1,3 +1,5 @@
+
+/*
 var x:number=12_32_45
 var y:string="charan"
 var z:boolean=true
@@ -6,4 +8,15 @@ var a ;
 a=1
 a="charan"
 console.log(typeof(a));
+
+*/
+
+// Arrays in TS
+
+var n:number[]=[]
+n.push(12,2,3,7,4)
+console.log(n);
+console.log(typeof(n.join("hello")));// it will add element in between array elements and convert array to string 
+console.log(n.lastIndexOf(1));
+
 
